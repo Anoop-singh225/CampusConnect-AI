2234de806cb6a327a9e2f80f9bf93b225eacd66c
Anoop singh <sanoop6027@gmail.com>
2026-04-26 17:31:26 +0530

    Add files via upload

--- src/app/api/resume/route.ts
+++ src/app/api/resume/route.ts
@@ -38,7 +38,7 @@ export async function GET(request: Request) {
     `;
 
     const response = await ai.models.generateContent({
-        model: 'gemini-2.5-flash',
+        model: 'gemini-1.5-flash',
         contents: promptData,
     });
     
